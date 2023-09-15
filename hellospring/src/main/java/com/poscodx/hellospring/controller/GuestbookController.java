1a57c5d199ca93163a9801a752452db055d34558
thddb <thddb@YUNA-GB3>
2023-09-15 09:24:07 +0900

    spring basic 1

---
+++ hellospring/src/main/java/com/poscodx/hellospring/controller/GuestbookController.java
@@ -0,0 +1,21 @@
+package com.poscodx.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+// @RequestMapping 클래스 단독 매핑
+@Controller
+@RequestMapping("/guestbook/*")
+public class GuestbookController {
+	@ResponseBody
+	@RequestMapping
+	public String list() {
+		return "GuestbookController.list()";
+	}
+
+	@RequestMapping
+	public String delete() {
+		return "GuestbookController.delete()";
+	}
+}
