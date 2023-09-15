86c60f57910b56cfba35323d1ee6af75b52c192c
thddb <thddb@YUNA-GB3>
2023-09-15 09:54:38 +0900

    @Request Mapping

--- hellospring/src/main/java/com/poscodx/hellospring/controller/GuestbookController.java
+++ hellospring/src/main/java/com/poscodx/hellospring/controller/GuestbookController.java
@@ -1,11 +1,11 @@
 package com.poscodx.hellospring.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 // @RequestMapping 클래스 단독 매핑
-@Controller
+// Spring mvb 4.x 버전에서만 지원
+// @Controller
 @RequestMapping("/guestbook/*")
 public class GuestbookController {
 	@ResponseBody
