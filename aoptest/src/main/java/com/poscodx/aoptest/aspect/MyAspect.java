5faf11f3d87f63d497c519b6898f14d5bddeebae
thddb <thddb@YUNA-GB3>
2023-09-19 11:22:26 +0900

    AOP Programming 1

--- aoptest/src/main/java/com/poscodx/aoptest/aspect/MyAspect.java
+++ aoptest/src/main/java/com/poscodx/aoptest/aspect/MyAspect.java
@@ -1,8 +1,10 @@
 package com.poscodx.aoptest.aspect;
 
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -21,13 +23,29 @@ public class MyAspect {
 		System.out.println("---- After Advice ----");
 	}
 
-	@AfterReturning("execution(* com.poscodx.aoptest.service.ProductService.find(String))")
+	@AfterThrowing(value = "execution(* *..*.*.*(..))", throwing = "ex")
+	public void adviceAfterThrowing(Throwable ex) {
+		System.out.println("---- AfterThrowing Advice : " + ex + " ----");
+	}
+
+	@AfterReturning("execution(* *.ProductService.find(String))")
 	public void adviceAfterReturning() {
 		System.out.println("---- AfterReturning Advice ----");
 	}
 
-	@AfterThrowing(value = "execution(* *..*.ProductService.find(..))", throwing = "ex")
-	public void adviceAfterThrowing(Throwable ex) {
-		System.out.println("---- AfterThrowing Advice : " + ex + " ----");
+	@Around("execution(* *..*.*.*(..))")
+	public Object adviceAround(ProceedingJoinPoint pjp) throws Throwable {
+		// Before
+		System.out.println("---- Around(before) Advice ----");
+
+		// Point Cut Method 실행
+//		Object[] params = {"Camera", };
+//		Object result = pjp.proceed(params);
+		Object result = pjp.proceed();
+
+		// After
+		System.out.println("---- Around(after) Advice ----");
+		
+		return result;
 	}
 }
--- aoptest/src/main/java/com/poscodx/aoptest/service/ProductService.java
+++ aoptest/src/main/java/com/poscodx/aoptest/service/ProductService.java
@@ -7,7 +7,7 @@ import com.poscodx.aoptest.vo.ProductVo;
 @Service
 public class ProductService {
 
-	public ProductVo find(String keyword) throws Exception {
+	public ProductVo find(String keyword) {
 		ProductVo vo = new ProductVo(keyword);
 
 //		if (1 - 1 == 0) {
