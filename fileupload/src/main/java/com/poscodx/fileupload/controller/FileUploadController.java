bfc756a6bcdd21c4b625b4d20a861b43a3899807
thddb <thddb@YUNA-GB3>
2023-09-21 14:59:43 +0900

    fileupload

---
+++ fileupload/src/main/java/com/poscodx/fileupload/controller/FileUploadController.java
@@ -0,0 +1,31 @@
+package com.poscodx.fileupload.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.poscodx.fileupload.service.FileUploadService;
+
+@Controller
+public class FileUploadController {
+	@Autowired
+	private FileUploadService fileUploadService;
+	
+	@RequestMapping("/form")
+	public String form() {
+		return "form";
+	}
+
+	@RequestMapping(value = "/upload", method = RequestMethod.POST)
+	public String upload(@RequestParam("e") String email, @RequestParam("f") MultipartFile file, Model model) {
+		// image file upload
+		String url = fileUploadService.restore(file);
+		
+		model.addAttribute("url", url);
+		return "result";
+	}
+}
