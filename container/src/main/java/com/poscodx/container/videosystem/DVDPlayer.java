fc8ea70ca3696e87809e696f3e3c7f6792261900
SongYuna99 <thddbsk7248@gmail.com>
2023-10-05 10:11:31 +0900

    container : Explicit (Java Configuration) finish

--- container/src/main/java/com/poscodx/container/videosystem/DVDPlayer.java
+++ container/src/main/java/com/poscodx/container/videosystem/DVDPlayer.java
@@ -7,8 +7,15 @@ import org.springframework.stereotype.Component;
 public class DVDPlayer {
 	@Autowired
 	private DigitalVideoDisc dvd;
-
+	
+	public DVDPlayer() {
+	}
+	
 	public DVDPlayer(DigitalVideoDisc dvd) {
+		this.dvd = dvd;
+	}
+
+	public void setDvd(DigitalVideoDisc dvd) {
 		this.dvd = dvd;
 	}
 
